b7c88f07ee867a958e9df20490cd841e5c8b4947
Lody Borgers <l.borgers@taf.nl>
2023-06-08 23:14:45 +0200

    Added an admin user guard to some routes

--- app/routes/bbq.new.tsx
+++ app/routes/bbq.new.tsx
@@ -1,6 +1,13 @@
+import type { LoaderArgs } from '@remix-run/node';
+import { requireAdminUser } from '~/session.server';
 import MainLayout from '~/layouts/Main';
 import Navigation from '~/components/Navigation';
 import BBQForm from '~/components/BBQForm';
+
+export async function loader({ request }: LoaderArgs) {
+  await requireAdminUser(request);
+  return null;
+}
 
 export default function NewBBQRoute() {
   return (
